baa38888b813725636fe2a3e7ee9d2914a148310
Sumit Kumar <sumit21adm@gmail.com>
2022-04-02 21:07:04 +0530

    Ecomm_Project_Django Files Upload/Updated

---
+++ Ecomm_Project_Django/EcommProj/ecomm/ecommapp/utils.py
@@ -0,0 +1,7 @@
+def upload_file(f):
+    print(f)
+    with open('ecommapp/uploads/' + f.name, 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+            
